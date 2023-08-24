5e6790e03c75e380169a351301cf954b5e8cd05a
Emanoel Lopes <emanoelopes@gmail.com>
2023-08-24 05:31:18 -0300

    adding .gitignore

---
+++ udemy_python_para_todos/aula_poo_ex1/sistema.py
@@ -0,0 +1,8 @@
+from heroi import Heroi
+
+# Heroi(voa, possui_arma, lanca_teia, frase_comum)
+homem_aranha = Heroi(False, False, True, "")
+homem_aranha.detalhar()
+
+he_man = Heroi(False, True, False, "Eu tenho a força")
+he_man.falar()
