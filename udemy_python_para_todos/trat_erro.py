c4f6deae03c0ac20aefcd5e71437f3609b2d5799
Emanoel Lopes <emanoelopes@gmail.com>
2023-10-03 05:37:36 -0300

    update

---
+++ udemy_python_para_todos/trat_erro.py
@@ -0,0 +1,10 @@
+valor = input("Informe um número")
+
+try:
+    soma = int(valor) + 10
+    resultado = soma / int(valor)
+except ZeroDivisionError:
+    print("Ocorreu um erro de divisão por zero.")
+except ValueError:
+    print("Ocorreu um erro de valor.")
+
