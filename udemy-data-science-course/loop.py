ba0cb984485220e9eab5ca6efb23c6295fed3b2c
Emanoel Lopes <emanoelopes@gmail.com>
2025-04-24 11:01:51 -0300

    updates

--- udemy-data-science-course/loop.py
+++ udemy-data-science-course/loop.py
@@ -5,3 +5,15 @@ x = 1
 while x < 30:
     print(x, end = " ")
     x += 2
+
+# Use conditional Statements and Loops
+
+for n in range(10):
+    print (2 ** n, end = " ")
+
+for x in range(20):
+    if x % 2 == 0:
+        print( x, end = " ")
+    else:
+        print ("Odd", end = " ")
+
