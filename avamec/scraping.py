c2ace98d02d9f690f9c4bb82bdd0502356d59b77
Emanoel Lopes <emanoelopes@gmail.com>
2025-08-31 18:06:17 -0300

    update

--- avamec/scraping.py
+++ avamec/scraping.py
@@ -7,5 +7,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Exemplo: extrair todos os títulos das atividades
-for titulo in soup.find_all("h3"):
+for titulo in soup.find_all("MuiTypography-root-118 jss161 MuiTypography-body1-120"):
     print(titulo.get_text(strip=True))
+    # Exibir o conteúdo HTML da página para inspeção
+    print(soup.prettify())
