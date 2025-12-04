9321c737f23748ee9b6452d02eb57ea45afde2fc
Emanoel Lopes <emanoelopes@gmail.com>
2025-12-03 23:03:14 -0300

    proditec repo created to host avamec subdirectory

--- avamec/bot_whatsapp/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from bot-whatsapp!")
-
-
-if __name__ == "__main__":
-    main()
--- avamec/bot_whatsapp/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-firefox:4.38.0-20251101
